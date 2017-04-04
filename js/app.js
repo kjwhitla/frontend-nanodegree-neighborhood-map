5435028cfe936a2d4680cdc233b50be725a481a6
Keegan Whitla <kjwhitla@gmail.com>
2017-04-04 16:05:35 -0700

    feat: info box opens and displays Information box for each opens and displays. However, it is possible to open all at once. Only one should be targeted.

--- js/app.js
+++ js/app.js
@@ -30,22 +30,48 @@ function initMap() {
 
     //Create Location Object      
     var Location = function(data) {
+        var marker;
         this.title = ko.observable(data.title); 
-
         this.lat = ko.observable(data.location.lat);
         this.lng = ko.observable(data.location.lng);
         this.address = ko.observable(data.address);
-        var marker = new google.maps.Marker({
+        marker = new google.maps.Marker({
             position: new google.maps.LatLng(data.location.lat, data.location.lng),
-            title: this.title,
+            title: data.title,
+            address: data.address,
             map: map
           });
+        
+        function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+            if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '<br/>' + marker.address + '</div>');
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+                infowindow.addListener('closeclick', function() {
+                 infowindow.marker = null;
+                });
+            }
+        }
+        
+        var largeInfowindow = new google.maps.InfoWindow();
+
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
 
         // Set the marker as a knockout observable
         this.marker = ko.observable(marker);
     };
     
     ko.applyBindings( new LocationViewModel() );
-
+    
+     
+    
+    
+    
+//intital map end    
 }
 
