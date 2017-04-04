b666713f9f8d31923c62edc658b519d52d9eba18
Keegan Whitla <kjwhitla@gmail.com>
2017-04-04 13:56:14 -0700

    feat: moved locations into model.js Moved the Google Maps code into app.js

---
+++ locations.js
@@ -0,0 +1,71 @@
+var locationsArray=[ {
+    title: 'Qs Halal Chicken',
+    location: {
+        lat: 37.7647818268752, lng: -122.2454328766948
+    },
+    address: '2306 Central Ave, Alameda, CA 94501'
+}
+
+,
+{
+    title: 'Scolaris Good Eats',
+    location: {
+        lat: 37.762817, lng: -122.244392
+    },
+    address: '1303 Park St, Alameda, CA 94501'
+}
+
+,
+{
+    title: 'Sidestreet Pho',
+    location: {
+        lat: 37.762783, lng: -122.245213
+    },
+    address: '2304 Encinal Ave, Alameda, CA 94501'
+}
+
+,
+{
+    title: 'Mama Papa Lithuania',
+    location: {
+        lat: 37.762029, lng: -122.244925
+    },
+    address: '1241 Park St, Alameda, CA 94501'
+}
+
+,
+{
+    title: 'Blue Danube',
+    location: {
+        lat: 37.763386, lng: -122.243859
+    },
+    address: '1333 Park St, Alameda, CA 94501'
+}
+
+,
+{
+    title: 'Yumi Sushi',
+    location: {
+        lat: 37.764834, lng: -122.242019
+    },
+    address: '1428 Park St B, Alameda, CA 94501'
+}
+
+,
+{
+    title: 'American Oak',
+    location: {
+        lat: 37.765856, lng: -122.242368
+    },
+    address: '2319 Santa Clara Ave, Alameda, CA 94501'
+}
+
+,
+{
+    title: 'Speisekammer',
+    location: {
+        lat: 37.765973, lng: -122.240179
+    },
+    address: '2424 Lincoln Ave, Alameda, CA 94501'
+}
+];
