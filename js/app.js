e4bf6af6b651f100563cb1470640a4c4698a3e03
Keegan Whitla <kjwhitla@gmail.com>
2017-04-05 15:04:48 -0700

    refactor: changed code to conform to MVVM structure Refactored code to support MVVM strucutre, broke everything in the process. Moved the largeInfoWindow, LocationViewModel, Location object of initMap

--- js/app.js
+++ js/app.js
@@ -2,24 +2,50 @@
 
 var map;
 
-function initMap() {
-    //Create map options
-    var mapOptions ={
-        center: {lat:  37.762906, lng: -122.244408},
-        zoom: 15,
-        mapTypeControl: false,
-        mapTypeId: 'roadmap'
-    };
+//Info window moved outside of object, this makes sure only one box can be opened at a time.        
+var largeInfowindow = new google.maps.InfoWindow({
+    content: '<div><p>This is the default content</p></div>'
+});
+
+//Create Location Object      
+var Location = function(data) {
+    var marker;
+    this.title = ko.observable(data.title); 
+    this.lat = ko.observable(data.location.lat);
+    this.lng = ko.observable(data.location.lng);
+    this.address = ko.observable(data.address);
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.location.lat, data.location.lng),
+        title: data.title,
+        address: data.address,
+        map: map
+      });
+
+    function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '<br/>' + marker.address + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+             infowindow.marker = null;
+            });
+        }
+    }
 
 
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    
-    //Info window moved outside of object, this makes sure only one box can be opened at a time.        
-    var largeInfowindow = new google.maps.InfoWindow();
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+    });
+
+    // Set the marker as a knockout observable
+    this.marker = ko.observable(marker);
+};
 
 
-    //Create the Location Movel View from the locations array
-    var LocationViewModel = function() {
+var LocationViewModel = function() {
         var self = this;
 
         //create a location array
@@ -31,46 +57,28 @@ function initMap() {
         });
     };
 
-    //Create Location Object      
-    var Location = function(data) {
-        var marker;
-        this.title = ko.observable(data.title); 
-        this.lat = ko.observable(data.location.lat);
-        this.lng = ko.observable(data.location.lng);
-        this.address = ko.observable(data.address);
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(data.location.lat, data.location.lng),
-            title: data.title,
-            address: data.address,
-            map: map
-          });
-        
-        function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-            if (infowindow.marker != marker) {
-            infowindow.marker = marker;
-            infowindow.setContent('<div>' + marker.title + '<br/>' + marker.address + '</div>');
-            infowindow.open(map, marker);
-            // Make sure the marker property is cleared if the infowindow is closed.
-                infowindow.addListener('closeclick', function() {
-                 infowindow.marker = null;
-                });
-            }
-        }
 
-
-        // Create an onclick event to open an infowindow at each marker.
-        marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-        });
-
-        // Set the marker as a knockout observable
-        this.marker = ko.observable(marker);
+function initMap() {
+    //Create map options
+    var mapOptions ={
+        center: {lat:  37.762906, lng: -122.244408},
+        zoom: 15,
+        mapTypeControl: false,
+        mapTypeId: 'roadmap'
     };
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
     
-    ko.applyBindings( new LocationViewModel() );
+
+
+
+    //Create the Location ModelView from 
+   
+
     
     
 //intital map end    
 }
 
+//Launch everything :)    
+ko.applyBindings( new LocationViewModel() );
